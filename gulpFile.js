933b7e3f0bb42c0f5fee4c38485bc887fd90faef
Tyler Garrett <tyler@tgrrtt.com>
2014-10-21 13:05:43 -0700

    Annotated & fixed gulpfile

--- gulpFile.js
+++ gulpFile.js
@@ -3,8 +3,9 @@
   var $     = require('gulp-load-plugins')({lazy:false});
   var del   = require('del');
   var wiredep = require('wiredep');
+  $.livereload();
 
-
+// Set up paths hashes for client directory
 var paths = {
   index: './client/index.html',
   root: './client',
@@ -16,6 +17,7 @@ var paths = {
   templates: ['./client/**/*.html', '!./client/bower_components/**/*.html']
 };
 
+// Set up paths hashes for paths directory
 var dist = {
   root: './dist',
   index: './dist/index.html',
@@ -27,10 +29,7 @@ var dist = {
 
 };
 
-var templates = {
- controller: './templates/*'
-};
-
+// options that we can easily reference later on.
 var options = {
   templates: {
     name: 'templates.js',
@@ -41,56 +40,101 @@ var options = {
   inject: {
     scripts: {
       relative: true,
-      name: "app",
+      name: 'app',
 
     },
     styles: {
       relative: true,
-      name: "styles"
+      name: 'styles'
     }
   }
 }
-// put colon:dev on stuff that doenst have a :dist on it
-gulp.task('default', $.sequence('inject', 'wire', 'server', 'watch'));
-gulp.task('server', startServerDev);
-gulp.task('watch', startWatch);
-gulp.task('inject', startInject);
-gulp.task('wire', wireBower);
-// ==================================
-gulp.task('controller', makeController);
-gulp.task('dist', $.sequence('build', 'server:dist'));
-gulp.task('server:dist', startServerProd);
+// Lets declare our actual tasks & what they do below
+//=========================================================
+
+// Dev tasks - wont 'build' anything aka doesn't dump into dist folder.
+//==================================
+gulp.task('default', $.sequence('inject:dev', 'wire:dev', 'server:dev', 'watch:dev'));
+// serves stuff in the client folder
+gulp.task('server:dev', startServerDev);
+// watches for changes in client folder
+gulp.task('watch:dev', startWatchDev);
+// injects our css & js into index.html
+gulp.task('inject:dev', injectDev);
+// injects bower stuff into index.html
+gulp.task('wire:dev', wireBower);
+
+// Dist tasks - reads from client folder and shits out stuff into the dist (production ready) folder
+//===================================
+
+// builds from client folder -> dist folder
 gulp.task('build', $.sequence('clean','copy', ['templates:dist','styles:dist','scripts:dist','image:dist','bower:dist'], 'inject:dist', 'wire:dist'));
+// default dist task: builds, serves & watches
+gulp.task('dist', $.sequence('build', 'server:dist', 'watch:dist'));
+// serve up files from the dist directory
+gulp.task('server:dist', startServerProd);
+// wire bower deps into index.html
 gulp.task('wire:dist', wireBowerDist);
-gulp.task('styles:dist', distStyles);
-gulp.task('bower:dist', distBowerFiles);
+// watch for changes in our dist directory
+gulp.task('watch:dist', startWatchDist);
+// minify, etc css
+gulp.task('styles:dist', stylesDist);
+// moves bower stuff over into dist folder
+gulp.task('bower:dist', bowerFilesDist);
+// injects our css and js files into index.html
 gulp.task('inject:dist', injectDist);
-gulp.task('scripts:dist', distScripts);
-gulp.task('image:dist', distImages);
+// does stuff to our js
+gulp.task('scripts:dist', scriptsDist);
+// minifyt & moves images
+gulp.task('image:dist', imagesDist);
+// copies all other files into the dist folder
 gulp.task('copy',copyFiles);
-gulp.task('templates:dist', distTemplates);
+// creates a giant folder of all of our angular partials
+gulp.task('templates:dist', templatesDist);
+// empties out the entire dist folder, with the exception of any git files (cuz dist is a git repo as well)
 gulp.task('clean', del.bind(null, ['!.git*','./dist/**/*']));
 
+// ===============================
+// The following are what actually happens when a specific gulp task happens
+// ===============================
 
+// Server functions
+//============================
 function startServerDev(){
+  // set dev env so dist folder isnt served
   process.env.NODE_ENV = 'development';
   require('./server');
 
 }
+
 function startServerProd(){
+  // set prod env so dist folder is served
   process.env.NODE_ENV = 'production';
   require('./server');
 
 }
-function startWatch(){
-  $.livereload();
+// Watch functions
+//============================
+function startWatchDev(){
   $.livereload.listen();
-  gulp.watch('./client/app/**/*.css', $.livereload.changed);
-  gulp.watch('./client/app/**/*.js', $.livereload.changed);
-  gulp.watch('./client/**/*.html', $.livereload.changed);
+  // watch for changes in these files and let livereload know something happened
+  gulp.watch(paths.root + '/app/**/*.css', $.livereload.changed);
+  gulp.watch(paths.root + '/app/**/*.js', $.livereload.changed);
+  gulp.watch(paths.root + '/**/*.html', $.livereload.changed);
 }
 
-function startInject(){
+function startWatchDist(){
+  // NOTE THIS CURRENTLY DOESNT WORK
+  // it watches the main folder but nothing actually builds :(
+  $.livereload.listen();
+  gulp.watch(paths.root + '/app/**/*.css', $.livereload.changed);
+  gulp.watch(paths.root + '/app/**/*.js', $.livereload.changed);
+  gulp.watch(paths.root + '/**/*.html', $.livereload.changed);
+
+}
+//inject functions
+//===============================
+function injectDev(){
   var target  = gulp.src( paths.index );
   var scripts = gulp.src( paths.scripts, {read:false} );
   var styles  = gulp.src( paths.styles, {read:false} );
@@ -100,6 +144,19 @@ function startInject(){
     .pipe( $.inject( styles,  options.inject.styles ) )
     .pipe( gulp.dest( paths.root ) );
 }
+
+function injectDist(){
+  var target = gulp.src( dist.index ) ;
+  var scripts = gulp.src( dist.scripts + '/*.js' );
+  var styles = gulp.src( dist.styles + '/*.css' );
+
+  return target
+    .pipe( $.inject( scripts,  options.inject.scripts ) )
+    .pipe( $.inject( styles,  options.inject.styles ) )
+    .pipe(gulp.dest(dist.root));
+}
+// Wire up bower deps (inject but for bower)
+//=============================
 function wireBower(){
   var wire = wiredep.stream;
   return gulp.src(paths.index)
@@ -114,51 +171,43 @@ function wireBowerDist(){
     .pipe(gulp.dest(dist.root));
 
 }
-function makeController(){
-  var values = {};
-  // console.log(this.tasks.controller.args);
-  // console.log(JSON.stringify(arguments[0]);
-  // values.name = this.args];
-  return gulp.src(templates.controller)
-    // .pipe($.template(values))
-    .pipe($.rename('about.controller.js'))
-    .pipe(gulp.dest('./client/app/core/controllers'));
-}
-
-function distStyles() {
+// Styles 4 Dist
+// ============================
+function stylesDist() {
   return gulp.src(paths.styles)
     .pipe($.concat('app.min.css'))
     .pipe(gulp.dest(dist.styles));
 }
-function distScripts() {
+// Do stuff to our scripts
+//=====================================
+function scriptsDist() {
   return gulp.src(paths.scripts)
     .pipe($.concat('app.min.js'))
     .pipe(gulp.dest(dist.scripts));
 }
+// Task that copies all files in our client root folder (not anything inside a folder)
+// ====================================
 function copyFiles() {
   return gulp.src(paths.root + '/*.*')
     .pipe(gulp.dest(dist.root));
 }
-function distBowerFiles() {
+// Move all of our bower files over to dist
+//======================================
+function bowerFilesDist() {
   return $.bower(paths.bower)
   .pipe(gulp.dest(dist.bower));
 }
-function injectDist(){
-  var target = gulp.src( dist.index ) ;
-  var scripts = gulp.src( dist.scripts + '/*.js' );
-  var styles = gulp.src( dist.styles + '/*.css' );
 
-  return target
-    .pipe( $.inject( scripts,  options.inject.scripts ) )
-    .pipe( $.inject( styles,  options.inject.styles ) )
-    .pipe(gulp.dest(dist.root));
-}
-function distImages() {
+// Minify and move all images over
+//=====================================
+function imagesDist() {
   return gulp.src(paths.images)
     .pipe($.image())
     .pipe(gulp.dest(dist.images));
 }
-function distTemplates() {
+// Take all of our angular partials, and put them all in one 'template script'
+//=======================================
+function templatesDist() {
   return gulp.src(paths.templates)
     .pipe($.angularTemplatecache(options.templates.name, options.templates.options))
     .pipe(gulp.dest(dist.scripts));
