19a0e6a1e35d8d36280c5adebb6c7e2ed13c53fd
Tyler Garrett <tyler@tgrrtt.com>
2014-10-20 20:21:26 -0700

    Newly scaffoled app using yeoman-angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gifchatClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gifchatClientApp
+ */
+angular.module('gifchatClientApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
