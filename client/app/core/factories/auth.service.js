8174bd1f8df5dbd46ca6a97a1f7b0f20248b7917
Tyler Garrett <tyler@tgrrtt.com>
2014-10-23 12:03:07 -0700

    Auth service complete

--- client/app/core/factories/auth.service.js
+++ client/app/core/factories/auth.service.js
@@ -12,9 +12,6 @@ angular.module('gifchatClientApp')
       /**
        * Authenticate user and save token
        *
-       * @param  {Object}   user     - login info
-       * @param  {Function} callback - optional
-       * @return {Promise}
        */
       login: function(user) {
 
@@ -35,7 +32,6 @@ angular.module('gifchatClientApp')
       /**
        * Delete access token and user info
        *
-       * @param  {Function}
        */
       logout: function() {
         $cookieStore.remove('token');
@@ -45,9 +41,6 @@ angular.module('gifchatClientApp')
       /**
        * Create a new user
        *
-       * @param  {Object}   user     - user info
-       * @param  {Function} callback - optional
-       * @return {Promise}
        */
       createUser: function(user, callback) {
         return $http.post('http://gifserver.azurewebsites.net/users/signup', {
@@ -60,31 +53,10 @@ angular.module('gifchatClientApp')
         });
       },
 
-      /**
-       * Change password
-       *
-       * @param  {String}   oldPassword
-       * @param  {String}   newPassword
-       * @param  {Function} callback    - optional
-       * @return {Promise}
-       */
-      // changePassword: function(oldPassword, newPassword, callback) {
-      //   var cb = callback || angular.noop;
-
-      //   return User.changePassword({ id: currentUser._id }, {
-      //     oldPassword: oldPassword,
-      //     newPassword: newPassword
-      //   }, function(user) {
-      //     return cb(user);
-      //   }, function(err) {
-      //     return cb(err);
-      //   }).$promise;
-      // },
 
       /**
        * Gets all available info on authenticated user
        *
-       * @return {Object} user
        */
       getCurrentUser: function() {
         return currentUser;
@@ -93,10 +65,8 @@ angular.module('gifchatClientApp')
       /**
        * Check if a user is logged in
        *
-       * @return {Boolean}
        */
       isLoggedIn: function() {
-        // if (token) return true;
         if ($cookieStore.get('token')) {
           return true;
         } else {
@@ -104,31 +74,6 @@ angular.module('gifchatClientApp')
         }
       },
 
-      /**
-       * Waits for currentUser to resolve before checking if user is logged in
-       */
-      isLoggedInAsync: function(cb) {
-        if(currentUser.hasOwnProperty('$promise')) {
-          currentUser.$promise.then(function() {
-            cb(true);
-          }).catch(function() {
-            cb(false);
-          });
-        } else if(currentUser.hasOwnProperty('role')) {
-          cb(true);
-        } else {
-          cb(false);
-        }
-      },
-
-      /**
-       * Check if a user is an admin
-       *
-       * @return {Boolean}
-       */
-      isAdmin: function() {
-        return currentUser.role === 'admin';
-      },
 
       /**
        * Get auth token
